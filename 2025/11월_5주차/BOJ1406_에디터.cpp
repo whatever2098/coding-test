814f1b95d014a48fc11fe45b4347b9db509e818a
Hyunjin <hyunjin2098@gmail.com>
2025-11-25 14:19:52 +0900

    에디터 문제 solve

---
+++ 2025/11월_5주차/BOJ1406_에디터.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    int M;
+    cin >> M;
+
+    vector<char> left(s.begin(), s.end());
+    vector<char> right;
+
+    for (int i = 0; i < M; i++) {
+        char cmd;
+        cin >> cmd;
+
+        if (cmd == 'L') {
+            if (!left.empty()) {
+                right.push_back(left.back());
+                left.pop_back();
+            }
+        }
+        else if (cmd == 'D') {
+            if (!right.empty()) {
+                left.push_back(right.back());
+                right.pop_back();
+            }
+        }
+        else if (cmd == 'B') {
+            if (!left.empty()) {
+                left.pop_back();
+            }
+        }
+        else if (cmd == 'P') {
+            char x;
+            cin >> x;
+            left.push_back(x);
+        }
+    }
+
+    string ans;
+    ans.reserve(left.size() + right.size()); // (선택) 성능 미세 개선
+
+    for (char ch : left) {
+        ans += ch;
+    }
+    for (int i = (int)right.size() - 1; i >= 0; --i) {
+        ans += right[i];
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
--- practicing & debugging C++/prac.cpp
+++ practicing & debugging C++/prac.cpp
@@ -3,54 +3,53 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr); cout.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-	int T;
-	cin >> T;
-	while (T--) {
-		string s;
-		cin >> s;
+	string s;
+	cin >> s;
 
-		vector<char>left, right;  //두 개의 스택처럼 사용
+	int M;
+	cin >> M;
 
-		for (char c : s) {
-			if (c == '<') {
-				if (!left.empty()) {
-					right.push_back(left.back());
-					left.pop_back();
-				}
-			}
+	vector<char> left(s.begin(), s.end());
+	vector<char> right;
 
-			else if (c == '>') {
-				if (!right.empty()) {
-					left.push_back(right.back());
-					right.pop_back();
-				}
-			}
+	for (int i{ 0 }; i < M; i++) {
 
-			else if (c == '-') {
-				if (!left.empty()) {
-					left.pop_back();
-				}
-			}
-			else {
-				left.push_back(c);  //일반 문자: 커서 위치에 삽입 == 왼쪽 스택에 push
+		char cmd;
+		cin >> cmd;
+
+		if (cmd == 'L') {
+			if (!left.empty()) {
+				right.push_back(left.back());
+				left.pop_back();
 			}
 		}
-
-
-
-		//출력
-		string ans;
-		ans.reserve(left.size() + right.size());
-
-			for (char c : left) { ans.push_back(c); }
-			while (!right.empty()) {
-				ans.push_back(right.back());
+		else if (cmd == 'D') {
+			if (!right.empty()) {
+				left.push_back(right.back());
 				right.pop_back();
 			}
+		}
+		else if (cmd == 'B') {
+			if (!left.empty()) {
+					left.pop_back();
+			}
+		}
+		else if (cmd == 'P') {
+			char x;
+			cin >> x;
+			left.push_back(x);
 
-			cout << ans << "\n";
+		}
+	} 
+	string ans;
+	for (char ch : left) {
+		ans += ch;
 	}
+	for (int i{ (int)right.size() - 1 }; i >= 0; i--) {
+		ans += right[i];
+	}
+	cout << ans << "\n";
 }
