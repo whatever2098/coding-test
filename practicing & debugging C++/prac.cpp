70c785c3f983376dda456f2271fcc0a35cb875bc
Hyunjin <hyunjin2098@gmail.com>
2026-05-05 08:47:34 +0900

    save current work

--- practicing & debugging C++/prac.cpp
+++ practicing & debugging C++/prac.cpp
@@ -1,25 +1,40 @@
-#include <iostream>
+# include <vector>
+#include <queue>
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+int solution(vector<vector<int>> maps) {
+	
+	int n = maps.size();
+	int m = maps[0].size();
 
-	int n, m;
-	cin >> n >> m;
+	queue<pair<int, int>> q;
+	vector<vector<bool>> visited(n, vector<bool>(m, false));
 
-	int cnt{ 0 };
+	int dx[4] = {1, 0, -1, 0};
+	int dy[4] = { 0, -1, 0, 1 };
 
-	while (n < m) {
-		if (m % 2 == 0) {
-			m /= 2;
+	q.push({ 0, 0 });
+	visited[0][0] = true;
+
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+
+		for (int i{ 0 }; i < 4; i++) {
+			int new_x = x + dx[i];
+			int new_y = y + dy[i];
+
+			if (new_x < 0 || new_x >= n || new_y < 0 || new_y >= m) continue; // 벽이면 무시
+			if (maps[new_x][new_y] == 0) continue;
+			if (visited[new_x][new_y] == true) continue;
+			
+			visited[new_x][new_y] = true;
+			maps[new_x][new_y] = maps[x][y] + 1;
+			// 현재 칸까지의 거리 + 1을 다음 칸에 저장
+			
+			q.push({ new_x, new_y });
 		}
-		else {
-			m++;
-		}
-		cnt++;
 	}
-	cnt += (n - m);
-
-	cout << cnt;
+	
 }
