cffc4f365b88e423b590fdaadf8d3b7eaba485ab
Hyunjin <hyunjin2098@gmail.com>
2026-05-11 17:37:25 +0900

    solving BFS

--- practicing & debugging C++/prac.cpp
+++ practicing & debugging C++/prac.cpp
@@ -8,33 +8,23 @@ public:
 		int n = grid.size();
 		int m = grid[0].size();
 
-		int dx[4] = { 1, 0, -1, 0 };
-		int dy[4] = { 0, -1, 0, 1 };
+		int dx[4] = { 1, 0, -1, 0 };//0123
+		int dy[4] = { 0, -1, 0, 1 };//오아왼위
 
 		int minute{ 0 };
-		queue<pair<int, int>> q;
 		int fresh{ 0 };
-		
-		// 처음 썩은 오렌지는 큐에 넣는다.
+
+		queue<pair<int, int>> q;
+
 		for (int i{ 0 }; i < n; i++) {
 			for (int j{ 0 }; j < m; j++) {
-				if (grid[i][j] == 2) {
-					q.push({ i, j });
-				}
-				else if (grid[i][j] == 1) {
-					fresh++;
-				}
+				if (grid[i][j] == 1) fresh++;
+				else if (grid[i][j] == 2) q.push({ i, j });
 			}
 		}
-
-		if (fresh == 0) return 0;
-
-		while (!q.empty()) {
-
-			int size = q.size();
-			bool rotted = false;
-
-			for (int i{ 0 }; i < size; i++) {
+		int rotted = q.size();
+		while (!q.empty() && fresh) {
+			while (rotted--) {
 				int x = q.front().first;
 				int y = q.front().second;
 				q.pop();
@@ -42,20 +32,16 @@ public:
 				for (int dir{ 0 }; dir < 4; dir++) {
 					int nx = x + dx[dir];
 					int ny = y + dy[dir];
-
 					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 					if (grid[nx][ny] != 1) continue;
 
 					grid[nx][ny] = 2;
 					fresh--;
-					rotted = true;
 					q.push({ nx, ny });
 				}
+				minute++;
 			}
-			if(rotted) minute++;
 		}
-
-		return fresh == 0 ? minute : -1;
-		
+		return ((fresh) ? -1 : minute);
 	}
 };
